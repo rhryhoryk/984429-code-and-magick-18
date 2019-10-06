3aade646b78518abe0302b61e03e78c765c3876c
rhryhoryk <eh480.35@gmail.com>
2019-10-07 01:42:43 +0300

    pimp my mage

---
+++ js/backend.js
@@ -0,0 +1,40 @@
+'use strict';
+
+(function () {
+
+  window.backend = {
+    load: function (onLoad, onError) {
+      var url = 'https://js.dump.academy/code-and-magick/data';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.open('GET', url);
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var url = 'https://js.dump.academy/code-and-magick';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.open('POST', url);
+
+      xhr.addEventListener(onload, function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.send(data);
+    }
+  };
+})();
